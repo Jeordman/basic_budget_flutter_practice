d2656d8ebc25cb9fe8cd5d9741d91f89b4ecbb99
jeordman <callisterjeordin@gmail.com>
2022-01-28 14:51:13 -0500

    popup modal

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,105 @@
-import 'package:expense_tracker/widgets/user_transaction.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(_MyApp());
-}
+import './widgets/new_transaction.dart';
+import './widgets/transaction_list.dart';
+import './models/transactions.dart';
 
-class _MyApp extends StatelessWidget {
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
-        home: Scaffold(
-            appBar: AppBar(
-              title: const Text('Flutter app'),
+      title: 'Flutter App',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  // String titleInput;
+  // String amountInput;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final List<Transaction> _userTransactions = [
+    Transaction(
+      id: 't1',
+      title: 'New Shoes',
+      amount: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      id: 't2',
+      title: 'Weekly Groceries',
+      amount: 16.53,
+      date: DateTime.now(),
+    ),
+  ];
+
+  void _addNewTransaction(String txTitle, double txAmount) {
+    final newTx = Transaction(
+      title: txTitle,
+      amount: txAmount,
+      date: DateTime.now(),
+      id: DateTime.now().toString(),
+    );
+
+    setState(() {
+      _userTransactions.add(newTx);
+    });
+  }
+
+  void _startAddNewTransaction(BuildContext ctx) {
+    showModalBottomSheet(
+      context: ctx,
+      builder: (_) {
+        return GestureDetector(
+          onTap: () {},
+          child: NewTransaction(addTx: _addNewTransaction),
+          // catch tap outside of the modal sheet
+          behavior: HitTestBehavior.opaque,
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Flutter App'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.add),
+            onPressed: () => _startAddNewTransaction(context),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              width: double.infinity,
+              child: Card(
+                color: Colors.blue,
+                child: Text('CHART!'),
+                elevation: 5,
+              ),
             ),
-            body: SingleChildScrollView(
-                child: Column(
-              // vertical alignment
-              mainAxisAlignment: MainAxisAlignment.start,
-              // horizontal alignment
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: const <Widget>[
-                SizedBox(
-                  width: double.infinity,
-                  child: Card(
-                    color: Colors.blue,
-                    child: Text("CHART"),
-                  ),
-                ),
-                UserTransactions(),
-              ],
-            ))));
+            TransactionList(_userTransactions),
+          ],
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () => _startAddNewTransaction(context),
+      ),
+    );
   }
 }
--- lib/widgets/transaction_list.dart
+++ lib/widgets/transaction_list.dart
@@ -1,58 +1,65 @@
 import 'package:flutter/material.dart';
-import '../models/transactions.dart';
-// ignore: import_of_legacy_library_into_null_safe
 import 'package:intl/intl.dart';
+
+import '../models/transactions.dart';
 
 class TransactionList extends StatelessWidget {
   final List<Transaction> transactions;
 
-  const TransactionList(this.transactions, {Key? key}) : super(key: key);
-
-  convertDateTime(DateTime date) {
-    final DateFormat formatter = DateFormat('yyyy-MM-dd');
-    return formatter.format(date);
-  }
+  TransactionList(this.transactions);
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
+    return Container(
       height: 300,
-      //! EXTREMELY PERFORMANT
-      // LIST ONLY LOADS WHAT IS VISIBLE (GREAT FOR LARGE DATA SETS)
       child: ListView.builder(
-        itemBuilder: (context, index) => Card(
+        itemBuilder: (ctx, index) {
+          return Card(
             child: Row(
-          children: [
-            Container(
-                margin:
-                    const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black, width: 2)),
-                padding: const EdgeInsets.all(10),
-                child: Text(
-                  '\$${transactions[index].amount.toStringAsFixed(2)}',
-                  style: const TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 20,
-                    color: Colors.purple,
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.symmetric(
+                    vertical: 10,
+                    horizontal: 15,
                   ),
-                )),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  transactions[index].title,
-                  style: const TextStyle(
-                      fontSize: 16, fontWeight: FontWeight.bold),
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    ),
+                  ),
+                  padding: EdgeInsets.all(10),
+                  child: Text(
+                    '\$${transactions[index].amount.toStringAsFixed(2)}',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 20,
+                      color: Colors.purple,
+                    ),
+                  ),
                 ),
-                Text(
-                  convertDateTime(transactions[index].date),
-                  style: const TextStyle(color: Colors.grey),
-                )
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      transactions[index].title,
+                      style: TextStyle(
+                        fontSize: 16,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      DateFormat.yMMMd().format(transactions[index].date),
+                      style: TextStyle(
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
               ],
-            )
-          ],
-        )),
+            ),
+          );
+        },
         itemCount: transactions.length,
       ),
     );
